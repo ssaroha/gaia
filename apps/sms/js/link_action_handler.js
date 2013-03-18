f7bf56246a92be56f8fc495533362493e1d7f952
Satender Saroha <ssaroha@yahoo.com>
2013-03-18 13:56:05 +0530

    [Bug 838000] - Dialer invocation from message

--- apps/sms/js/link_action_handler.js
+++ apps/sms/js/link_action_handler.js
@@ -5,6 +5,7 @@
 */
 
 var LinkActionHandler = {
+  _l10n: navigator.mozL10n,
   handleBrowserEvent:
   function lah_handleBrowserEvent(link) {
     try {
@@ -20,18 +21,130 @@ var LinkActionHandler = {
     }
   },
 
+  handlePhoneEvent:
+  function lah_handlePhoneEvent(phoneNumber) {
+    this.call(phoneNumber);
+  },
+
+   handleLongPressPhoneEvent:
+  function lah_handleLongPressPhoneEvent(phoneNumber) {
+    var self = this;
+    var options = new OptionMenu({
+    'items': [
+      {
+        name: this._l10n.get('call'),
+        method: function optionMethod(param) {
+          self.call(param);
+        },
+        params: [phoneNumber]
+      },
+      {
+        name: this._l10n.get('createNewContact'),
+        method: function optionMethod(param) {
+          self.createNewContact(param);
+        },
+        params: [phoneNumber]
+      },
+      {
+        name: this._l10n.get('addToExistingContact'),
+        method: function optionMethod(param) {
+          self.addToExistingContact(param);
+        },
+        params: [phoneNumber]
+      },
+      {
+        name: this._l10n.get('cancel'),
+        method: function optionMethod(param) {
+          options.hide();
+        }
+      }
+    ],
+    'title': phoneNumber
+    });
+    options.show();
+  },
+
+   createNewContact: function lah_createNewContact(phoneNumber) {
+    try {
+      var activity = new MozActivity({
+        name: 'new',
+        data: {
+          type: 'webcontacts/contact',
+          params: {
+            'tel': phoneNumber
+          }
+        }
+      });
+    } catch (e) {
+      console.log('WebActivities unavailable? : ' + e);
+    }
+
+  },
+
+  addToExistingContact: function lah_addToExistingContact(phoneNumber) {
+    try {
+      var activity = new MozActivity({
+        name: 'update',
+        data: {
+          type: 'webcontacts/contact',
+          params: {
+            'tel': phoneNumber
+          }
+        }
+      });
+    } catch (e) {
+      console.log('WebActivities unavailable? : ' + e);
+    }
+  },
+
+  call: function lah_call(phoneNumber) {
+    try {
+      var activity = new MozActivity({
+        name: 'dial',
+        data: {
+          type: 'webtelephony/number',
+          number: phoneNumber
+        }
+      });
+    } catch (e) {
+      console.log('WebActivities unavailable? : ' + e);
+    }
+  },
+
   //Invokes handleBrowserEvent for now, and
   //in future will expand to call handlePhoneEvent,
   //handleEmailEvent.
-  handleEvent:
-   function lah_handleEvent(evt) {
+  handleTapEvent:
+   function lah_handleTapEvent(evt) {
      //Return if activity is already invoked
      if (this.activityInProgress) { return; }
      var eventAction = evt.target.dataset.action;
-        if (eventAction && eventAction === 'url-link') {
+     if (eventAction) {
+      switch (eventAction) {
+        case 'url-link':
           this.activityInProgress = true;
           this.handleBrowserEvent(evt.target.dataset.url);
-        }
+        break;
+        case 'phone-link':
+          this.activityInProgress = true;
+          this.handlePhoneEvent(evt.target.dataset.phonenumber);
+        break;
+      }
+    }
+  },
+
+  //Invokes handleLongPressPhoneEvent for now, and
+  //in future will expand to call handleLongPressEmailEvent
+  handleLongPressEvent:
+   function lah_handleLongPressEvent(evt) {
+     var eventAction = evt.target.dataset.action;
+     if (eventAction) {
+      switch (eventAction) {
+        case 'phone-link':
+          this.handleLongPressPhoneEvent(evt.target.dataset.phonenumber);
+        break;
+      }
+    }
   },
 
   resetActivityInProgress:
